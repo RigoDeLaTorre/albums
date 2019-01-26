06810fdc94f04892b1156a99865fed50cdfd8da5
Rigo De La Torre <rigoddd@gmail.com>
2019-01-26 10:20:49 -0800

    stling header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,26 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
-const Header = () => <Text>Albums</Text>;
+const Header = () => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: "#f8f8f8",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    paddingTop: 15
+  },
+  textStyle: {
+    fontSize: 40
+  }
+};
 
 export default Header;
