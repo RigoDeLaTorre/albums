9fbecb3209950d9032ab3dea2517e300387bf5db
Rigo De La Torre <rigoddd@gmail.com>
2019-01-26 21:04:26 -0800

    cardsection added

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,24 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import { View } from "react-native";
+import axios from "axios";
+import AlbumDetail from "./AlbumDetail";
 
-const AlbumList = () => (
-  <View>
-    <Text>Album List !!!</Text>
-  </View>
-);
+class AlbumList extends Component {
+  state = { albums: [] };
+  componentDidMount() {
+    axios
+      .get("https://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => this.setState({ albums: response.data }));
+  }
 
+  renderAlbums() {
+    return this.state.albums.map(album => (
+      <AlbumDetail key="album.title" album={album} />
+    ));
+  }
+  render() {
+    console.log(this.state);
+    return <View>{this.renderAlbums()}</View>;
+  }
+}
 export default AlbumList;
